1de2bce8c47efe03ae5cdf9bb00ced6246114dd7
kokosthief <kokosthief@gmail.com>
2024-01-30 21:38:12 +0100

    Homepage is live

---
+++ src/links.js
@@ -0,0 +1,48 @@
+export const mainContent = {
+  imgSrc: `/assets/profile.png`,
+  name: `Henry Willmott`,
+  tagline: `IRATA L1 | VCA-VOL | GWO FULL OFFSHORE`,
+};
+
+export const socialLinks = [
+  {
+    title: "Phone",
+    icon: "mdi:phone",
+    url: "https://api.whatsapp.com/send?phone=31683421604&_ga=2.113846863.934298603.1704190840-536151416.1704190839",
+  },
+  {
+    title: "Email",
+    icon: "mdi:mail",
+    url: "mailto:henry@hrwillmott.com",
+  },
+  {
+    title: "LinkedIn",
+    icon: "mdi:linkedin",
+    url: "https://www.linkedin.com/in/henrywillmott/",
+  },
+  {
+    title: "GitHub",
+    icon: "mdi:github",
+    url: "https://github.com/kokosthief",
+  },
+];
+
+export const links = [
+  // {
+  //   text: "Rope Access Market",
+  //   url: "https://ropeaccessmarket.com",
+  //   featured: true,
+  // },
+  // {
+  //   text: "About",
+  //   url: "https://jaydanurwin.com/about",
+  // },
+  // {
+  //   text: "Uses",
+  //   url: "https://jaydanurwin.com/uses",
+  // },
+  // {
+  //   text: "See my latest blog post!",
+  //   url: "https://jaydanurwin.com/about",
+  // },
+];
